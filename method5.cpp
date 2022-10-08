80c81e8211eba1f1d95352825730e29e6528daac
Breden <ebrede2@uic.edu>
2022-10-08 14:23:19 -0500

    update 5

--- method5.cpp
+++ method5.cpp
@@ -24,6 +24,7 @@ int main(int argc, char *argv[])
     double test = log2(n);
     cout << test << " " << floor(test) << endl;
     if (test - floor(test) != 0) {
+        cout << "Please pass power-of-2 input"
         return 0;
     }
 
